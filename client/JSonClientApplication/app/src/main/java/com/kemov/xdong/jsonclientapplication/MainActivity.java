11f122c60d5ce401708060937d0032bf500b406a
Jiang XD <jingxia_jxd@126.com>
2017-11-17 16:49:26 +0800

    add logic to get access to RESTful API user pwd
    Signed-off-by: Jiang XD <jingxia_jxd@126.com>

--- client/JSonClientApplication/app/src/main/java/com/kemov/xdong/jsonclientapplication/MainActivity.java
+++ client/JSonClientApplication/app/src/main/java/com/kemov/xdong/jsonclientapplication/MainActivity.java
@@ -1,12 +1,16 @@
 package com.kemov.xdong.jsonclientapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +19,7 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetAddress;
@@ -34,20 +39,69 @@ public class MainActivity extends AppCompatActivity {
     private EditText et_remark;
     private EditText et_addr;
     private String host="127.0.0.1";//同一个局域网内作为服务端的手机的IP，使用端口8155
-
+    String net_user = "";
+    String net_pwd = "";
+    String local_user = "";
+    String local_pwd = "";
     String modid;
     String name;
     String status;
     String remark;
+    private String m_user;
+    private String m_pwd;
+    private AlertDialog mDialog;
+    Handler netHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+
+            Log.i("kemov", "msg" + msg.what);
+            if (msg.what == 0x2) {
+                final String[] msg_net = ((String) msg.obj).split(" ");
+                net_user = msg_net[0];
+                net_pwd = msg_net[1];
+
+                if (net_user.equals(local_user) && net_pwd.equals(local_pwd))
+                    mDialog.dismiss();
+                else
+                    Toast.makeText(MainActivity.this, "用户名或密码错误", 0).show();
+            }
+
+            if (msg.what == 0x1) {
+                final String[] msg_local = ((String) msg.obj).split(" ");
+                local_user = msg_local[0];
+                local_pwd = msg_local[1];
+
+                new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            String response = MainActivity.this.get(" http://10.56.56.236:65500/appname/module/rest/user");
+                            JSONObject jsonObject = new JSONObject(response);
+                            Message message = Message.obtain();
+                            message.what = 0X2;
+                            message.obj = jsonObject.get("user").toString() + " " + jsonObject.getString("pwd".toString());
+                            netHandler.sendMessage(message);
+
+                        } catch (Exception e) {
+                            Log.i("kemov", e.toString());
+                        }
+                    }
+                }.start();
+            }
+            Log.i("kemov", "net:" + net_user + net_pwd + "local:" + local_user + local_pwd);
+        }
+
+    };
 
     Handler uiHandler = new Handler(){
         public void handleMessage(android.os.Message msg) {
+            Log.i("kemov", "msg" + msg.toString());
             if(msg.what==0x123)
             {
                 et_modid.setText(modid);
                 et_name.setText(name);
                 et_status.setText(status);
                 et_remark.setText(remark);
+
             }
         };
     };
@@ -56,6 +110,16 @@ public class MainActivity extends AppCompatActivity {
             = MediaType.parse("application/json; charset=utf-8");
 
     OkHttpClient client = new OkHttpClient();
+
+    String get(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
 
     String post(String url, String json) throws IOException {
         RequestBody body = RequestBody.create(JSON, json);
@@ -68,19 +132,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    String run(String url) throws IOException {
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         et_modid=(EditText) findViewById(R.id.EModID);
         et_name=(EditText) findViewById(R.id.EName);
@@ -89,42 +144,59 @@ public class MainActivity extends AppCompatActivity {
         et_addr = (EditText) findViewById(R.id.et_addr);
 
         Button b_send = (Button) findViewById(R.id.button_send);
-        Handler handler = new Handler() {
+
+        mDialog = new AlertDialog.Builder(this)
+                .setTitle("用户管理")
+                .setPositiveButton("登陆", null)
+                .setNegativeButton("注册", null)
+                .setCancelable(false)
+                .create();
+
+        LayoutInflater inflater = getLayoutInflater();
+        final View layout = inflater.inflate(R.layout.login_main, null);
+        final EditText et_relogin_name = (EditText) layout.findViewById(R.id.et_relogin_name);
+        final EditText et_relogin_pwd = (EditText) layout.findViewById(R.id.et_relogin_pwd);
+        mDialog.setView(layout);
+        mDialog.setOnShowListener(new DialogInterface.OnShowListener(){
             @Override
-            public void handleMessage(Message msg) {
-                super.handleMessage(msg);
-                Bundle data = msg.getData();
-                String val = data.getString("value");
-                Log.i("mylog", "请求结果为-->" + val);
-                // TODO
-                // UI界面的更新等相关操作
+            public void onShow(DialogInterface dialog) {
+                Button positionButton=mDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                Button negativeButton=mDialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+                positionButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String name = et_relogin_name.getText().toString();
+                        String pwd = et_relogin_pwd.getText().toString();
+                        Message message;
+                        if(name.isEmpty() || pwd.isEmpty())
+                            Toast.makeText(MainActivity.this, "请输入用户名和密码",0).show();
+                        else {
+
+                            message = Message.obtain();
+                            message.what = 0X1;
+                            message.obj = name.toString() + " " + pwd.toString();
+                            netHandler.sendMessage(message);
+                        }
+
+                    }
+                });
+                negativeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "目前不支持",0).show();
+                    }
+                });
             }
-        };
+        });
+
+        mDialog.show();
     };
 
     public void submit(View v) throws JSONException  {
+        Log.i("kemov", "click");
 
-        new Thread()  {
-            @Override
-            public void run()
-            {
-                try {
-                    String response = MainActivity.this.run("http://10.56.56.236:65500/todo/api/v1.0/tasks/3");
-                    Log.i("kemov", "respond" + response);
-                    JSONObject jsonObject=new JSONObject(response);
-
-                    modid=jsonObject.getString("modid");
-                    name=jsonObject.getString("name");
-                    status=jsonObject.getString("status");
-                    remark=jsonObject.getString("remark");
-                    Log.i("kemov", "we get " + modid + name + status + remark);
-                    uiHandler.sendEmptyMessage(0x123);
-
-                }catch (Exception e) {
-                }
-
-            }
-        }.start();
+        netHandler.sendEmptyMessage(0x1);
+    uiHandler.sendEmptyMessage(0x123);
 
 
     }
